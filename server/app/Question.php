38dc0dabfbd9b539e70c9dd23ff6b617e2991be0
cherbi_r <ruben.cherbit@epitech.eu>
2018-03-17 11:21:48 +0100

    Question Model

---
+++ server/app/Question.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Question extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'question'
+    ];
+    /**
+     * Get the answers for the Questions.
+     */
+    public function answers()
+    {
+        return $this->hasMany('App\Answer');
+    }
+}
