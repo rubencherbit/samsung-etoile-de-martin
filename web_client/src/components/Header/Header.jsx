dc07a4c4c519baaaa8ea008e15168c43b21e66f5
aldwin moutarlier <aldwin.moutarlier@oyez.fr>
2018-03-19 12:28:14 +0100

    fix HeaderComponents

--- web_client/src/components/Header/Header.jsx
+++ web_client/src/components/Header/Header.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
-import Styled from 'styled-components';
-class HeaderComponent extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
-    render() {
-        return (
-            <div className="header gid-x">
-                <img src="https://letoiledemartin.fr/wp-content/uploads/2018/01/logo_etoile_de_martin.jpg" alt="L'Etoile de Martin logo" />
-                <a className="don-btn" href="https://letoiledemartin.fr/nous-soutenir/faire-un-don-intro/" title="faire un don" target="_blank" rel="noopener noreferrer">don en ligne</a>
-            </div>
-        );
-    }
-}
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+const HeaderComponent = ({ className }) => (
+    <div className={className} >
+        <div className="header gid-x">
+            <img src="https://letoiledemartin.fr/wp-content/uploads/2018/01/logo_etoile_de_martin.jpg" alt="L'Etoile de Martin logo" />
+            <a className="don-btn" href="https://letoiledemartin.fr/nous-soutenir/faire-un-don-intro/" title="faire un don" target="_blank" rel="noopener noreferrer">don en ligne</a>
+        </div>
+    </div>
+)
 
-export default Styled(HeaderComponent)`
-		background-color:#fff;
+HeaderComponent.propTypes = {
+    className: PropTypes.string,
+};
+
+export default styled(HeaderComponent)`
+		background-color:black;
 		border:1px solid #e5e5e5;
 		box-shadow: 0 0 1px 0px rgba(0,0,0, .1);
 		align-items:center;
