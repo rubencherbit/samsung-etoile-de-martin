ab5a0512937b4d701a8d905b663a39ea425bfbf5
aldwin moutarlier <aldwin.moutarlier@oyez.fr>
2018-03-19 12:12:27 +0100

    add Styled in Header

--- web_client/src/components/Header/Header.jsx
+++ web_client/src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-
-export default class HeaderComponent extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+import Styled from 'styled-components';
+class HeaderComponent extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
     render() {
         return (
             <div className="header gid-x">
@@ -10,3 +10,43 @@ export default class HeaderComponent extends React.PureComponent { // eslint-dis
         );
     }
 }
+
+export default Styled(HeaderComponent)`
+		background-color:#fff;
+		border:1px solid #e5e5e5;
+		box-shadow: 0 0 1px 0px rgba(0,0,0, .1);
+		align-items:center;
+		.don-btn {
+			color:#fff;
+			text-decoration: none;
+            font-weight: bold;
+            text-tranform: capitalize;
+            font-size: .9rem;
+			background-color: #d13581;
+            float: right;
+            margin: 1rem 0rem 1rem 1rem;
+			padding: 10px;
+			border-radius: 2px;
+			box-shadow: 0 0 1px 0px rgba(0,0,0, .1);
+			border:1px solid #e5e5e5;
+			cursor: pointer;
+		}
+
+		img {
+			max-width: 100%;
+            width: 150px;
+            margin: 1rem 1rem 1rem 1rem;
+		}
+
+		& p {
+			font-size: .9rem;
+			background-color: #d13581;
+			margin 0;
+			padding: 10px;
+			border-radius: 2px;
+			box-shadow: 0 0 1px 0px rgba(0,0,0, .1);
+			border:1px solid #e5e5e5;
+			cursor: pointer;
+		}
+
+`;
--- web_client/src/global-styles.js
+++ web_client/src/global-styles.js
@@ -37,45 +37,6 @@ injectGlobal`
 		line-height: 1.5em;
 	}
 
-	.header {
-		background-color:#fff;
-		border:1px solid #e5e5e5;
-		box-shadow: 0 0 1px 0px rgba(0,0,0, .1);
-		align-items:center;
-		.don-btn {
-			color:#fff;
-			text-decoration: none;
-            font-weight: bold;
-            text-tranform: capitalize;
-            font-size: .9rem;
-			background-color: #d13581;
-            float: right;
-            margin: 1rem 0rem 1rem 1rem;
-			padding: 10px;
-			border-radius: 2px;
-			box-shadow: 0 0 1px 0px rgba(0,0,0, .1);
-			border:1px solid #e5e5e5;
-			cursor: pointer;
-		}
-
-		img {
-			max-width: 100%;
-            width: 150px;
-            margin: 1rem 1rem 1rem 1rem;
-		}
-
-		& p {
-			font-size: .9rem;
-			background-color: #d13581;
-			margin 0;
-			padding: 10px;
-			border-radius: 2px;
-			box-shadow: 0 0 1px 0px rgba(0,0,0, .1);
-			border:1px solid #e5e5e5;
-			cursor: pointer;
-		}
-
-	}
 
 	.container {
 		&-text {
